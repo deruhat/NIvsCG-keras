ebf3f79127a64fdc57100d5b04f290792d61c48d
deruhat <delohat@live.com>
2018-07-16 12:28:22 +0300

    fixed looping problem, fixed test labels problem

--- voting.py
+++ voting.py
@@ -20,6 +20,8 @@ from time import time
 from keras.callbacks import TensorBoard, ReduceLROnPlateau, ModelCheckpoint
 from PIL import Image
 
+np.set_printoptions(threshold=np.nan)
+
 # Load model
 model = load_model('NIvsCG_model.h5')
 
@@ -38,32 +40,30 @@ testTmp = []
 oriImageLabel = []  # one dimension list
 oriTestLabel = []  # one dimension list
 
-# TODO: implement majority voting algorithm and prediction using the model trained
-
+# Majority voting on prediction
 patchesTxt = 'utils/output-data/test/output.txt'
 patches = open(patchesTxt, 'r')
-i = 0
 
 print('Classifying patches...')
 for line in patches:
     patch = line.split()
-    if i < 200:
+    if len(patch) == 2:
+        # print('patch: ', patch)
         img = Image.open('utils/output-data/test/' + patch[0])
         img = img.resize((233, 233))
         imageTmp.append(int(patch[1]))
         arr = np.array((img_to_array(img)),)
         arr = arr.reshape((1, 233, 233, 3))
         prediction = model.predict(arr)
-        testTmp.append(prediction)
-        i = i + 1
+        testTmp.append(prediction[0][0])
     else:
+        # print('image is done')
         oriImageLabel.extend(imageTmp)
         oriTestLabel.extend(testTmp)
         imageLabel.append(imageTmp)
         testLabel.append(testTmp)
         imageTmp = []
         testTmp = []
-        i = 0
 
 print('The number of full-sized testing images is:', len(imageLabel))
 
@@ -73,10 +73,16 @@ imageLabelNp = np.array(imageLabel)
 testLabelNp = np.array(testLabel)
 
 #  Computing average accuracy on patches
-result = np.array(oriImageLabel) == np.array(oriTestLabel)
+oriTestLabel = np.rint(oriTestLabel)
 
-personal_result = result[:200*200] # (200 images * 200 patches per image)
-prcg_result = result[:200*200]
+result = (np.array(oriImageLabel) == np.array(oriTestLabel))
+# wierd = open("weird.txt", "w")
+# wierd.write(np.array2string(np.array(oriTestLabel)))
+
+kPrcgNum = 200  # the number of cg images
+
+personal_result = result[:kPrcgNum*200] # (200 images * 200 patches per image)
+prcg_result = result[kPrcgNum*200:]
 print('The number of patches:', len(oriImageLabel), len(prcg_result), len(personal_result))
 print('The average accuracy on patches:')
 print('The personal (NI) accuracy is:', personal_result.sum()*1.0/len(personal_result))
@@ -91,10 +97,10 @@ for x in range(len(imageLabel)):
     tmp = np.array(imageLabelNp[x]) == np.array(testLabelNp[x])
     result[x] = np.sum(tmp[:-1]) > imageCropNumNp[x]//2 - 1
 
-prcg_result = result[:200]
-personal_result = result[200:]
+personal_result = result[:kPrcgNum]
+prcg_result = result[kPrcgNum:]
 print('The average accuracy on full-sized images after majority voting: ', len(prcg_result), len(personal_result))
-print('The google (NI) accuracy is:', personal_result.sum()*1.0/len(personal_result))
+print('The personal (NI) accuracy is:', personal_result.sum()*1.0/len(personal_result))
 print('The prcg (CG) accuracy is:', prcg_result.sum()*1.0/len(prcg_result))
 print('CG images misclassified as natural images (CGmcNI) is:', (len(prcg_result) - prcg_result.sum())*1.0/len(prcg_result))
 print('natural images misclassified as CG images (NImcCG) is:', (len(personal_result) - personal_result.sum())*1.0/len(personal_result))
